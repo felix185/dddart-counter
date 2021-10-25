5063f2a4e99c39f190026377b0fe39d1207a45b8
Felix Riess <felix-riess@t-online.de>
2021-10-25 18:40:11 +0200

    Add common code

---
+++ code/common/src/main/java/de/riefel/dartcounter/common/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package holds common code for all modules (Bounded Contexts) of the application.
+ *
+ * @author Felix Riess <felix@felix-riess.de>
+ * @since 25 Okt 2021
+ */
+package de.riefel.dartcounter.common;
